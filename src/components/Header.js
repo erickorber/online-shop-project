43eac9cfa324fce453b84268a78b0d572fdc5176
Eric Korber <erickorber1994@gmail.com>
2018-05-18 12:47:32 -0400

    Add space on buttons
    Add margin space on the right side of the logo, which will only be noticeable with a desktop view. Add padding space on x-axis of menu buttons, to create more space between them, specifically when in a desktop view.

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = (props) => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
 
-      <button className="navbar-brand" type="button" value="Products" onClick={props.click}></button>
+      <button className="navbar-brand mr-5" type="button" value="Products" onClick={props.click}></button>
 
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
@@ -30,9 +30,9 @@ const Header = (props) => {
 
 function isCurrentPageActive(linkText, propsPageValue, clickFunction) {
   if (linkText === propsPageValue) {
-    return <button className="nav-link active mx-auto" value={linkText} onClick={clickFunction}>{linkText}</button>;
+    return <button className="nav-link active px-3 mx-auto" value={linkText} onClick={clickFunction}>{linkText}</button>;
   }
-  return <button className="nav-link mx-auto" value={linkText} onClick={clickFunction}>{linkText}</button>;
+  return <button className="nav-link px-3 mx-auto" value={linkText} onClick={clickFunction}>{linkText}</button>;
 }
 
 export default Header;
