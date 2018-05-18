babdaf51d3d85bc6317d0f4134178386ba11d904
Eric Korber <erickorber1994@gmail.com>
2018-05-18 11:38:53 -0400

    Add custom CSS for buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import '../css/Header.css';
 
 const Header = (props) => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">Shop Logo Image</a>
+
+      <button className="navbar-brand" type="button" value="Products" onClick={props.click}></button>
+
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
