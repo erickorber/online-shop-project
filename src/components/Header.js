6cc8775348a38292bc54da5ff4b67e35008d44bc
Eric Korber <erickorber1994@gmail.com>
2018-05-22 13:00:15 -0400

    Fix: Rename Products to ProductList for logo link as well

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = (props) => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
 
-      <button className="navbar-brand mr-5" type="button" value="Products" onClick={props.click}></button>
+      <button className="navbar-brand mr-5" type="button" value="ProductList" onClick={props.click}></button>
 
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
