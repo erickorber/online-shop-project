99618903313ebb535d06c9ba8147afe48af759af
Eric Korber <erickorber1994@gmail.com>
2018-05-24 15:55:15 -0400

    Add getPageToLoad method in App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,31 +1,56 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import Header from '../components/Header.js';
 import '../css/App.css';
+import Header from '../components/Header.js';
+import About from '../components/About.js'
+import Error404 from '../components/Error404.js'
+import Contact from '../components/Contact.js'
+import ProductList from '../components/ProductList.js'
+import Product from '../components/Product.js'
 
-import { setPageToLoadFromHeaderLink } from '../actions.js';
+import { setPageToLoad, setProductToLoad } from '../actions.js';
 
 const mapStateToProps = (state) => {
 	return {
-		pageValue: state.pageValue
+		pageValue: state.pageValue,
+    productId: state.productId
 	}
 }
 
 const mapDispatchToProps = (dispatch) => {
 	return {
-		onHeaderLinkClick: (clickedLink) => dispatch(setPageToLoadFromHeaderLink(clickedLink.target.value)) 
+		onLinkClick: (clickedLink) => dispatch(setPageToLoad(clickedLink.target.value)),
+    onProductClick: (id) => dispatch(setProductToLoad(id.target.value))  
 	}
 }
 
 class App extends Component {
 
+  getPageToLoad(page, productClick, id) {
+    switch (page) {
+      case 'About' :
+        return <About />
+      case 'Contact' :
+        return <Contact />
+      case 'Error404' :
+        return <Error404 />
+      case 'Product' : 
+        return <Product productId = {id} />
+      case 'ProductList' :
+        return <ProductList productClickFunction = {productClick} />
+      default:
+        return <Error404 />
+    }
+  }
+
   render() {
 
-  	const { pageValue, onHeaderLinkClick } = this.props;
+  	const { pageValue, onLinkClick, productId, onProductClick } = this.props;
 
     return (
       <div>
-        <Header page = {pageValue} click = {onHeaderLinkClick} />
+        <Header page = {pageValue} click = {onLinkClick} />
+        {this.getPageToLoad(pageValue, onProductClick, productId)}
       </div>
     );
   }
