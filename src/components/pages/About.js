233c9682ddf5441e5aa2af2d0cbfb68201117613
Eric Korber <erickorber1994@gmail.com>
2018-05-30 14:53:34 -0400

    Add HTML and CSS to page with template text values

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,8 +2,27 @@ import React from 'react';
 
 const About = (props) => {
 	return (
-		<div>
-About
+		<div className="container">
+			<div className="row page-title-spacing">
+				<div className="col-12">
+					<h2 className="text-center slight-shadow">About Page Title</h2>
+				</div>
+			</div>
+
+			<div className="row">
+				<div className="col-12">
+					<p className="mx-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+						pretium augue augue, a molestie leo sagittis sed. Proin at dui vitae
+						purus viverra fermentum quis vel augue. Sed fringilla non enim nec
+						bibendum. Pellentesque auctor egestas nunc ac lobortis. Nunc et massa
+						sit amet ante sodales hendrerit in at arcu. Mauris ullamcorper in
+						urna at tempor. Cras felis ipsum, maximus sed magna blandit, maximus
+						bibendum sem. Proin ornare porttitor ex et fermentum. Ut lectus elit,
+						ultrices nec diam nec, consequat imperdiet nibh. Nunc ut finibus
+						sapien. Etiam mauris nisi, auctor et turpis vitae, ullamcorper
+						gravida libero. </p>
+				</div>
+			</div>
 		</div>
 	);
 }
