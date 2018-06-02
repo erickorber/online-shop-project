cd45baa1530238a5c5010fc650850d7e898ee3db
Eric Korber <erickorber1994@gmail.com>
2018-06-02 11:56:04 -0400

    Setup HTML, CSS for contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import ContactForm from '../ContactForm.js';
 
 const Contact = (props) => {
 	return (
-		<div>
-		Contact
+		<div className="container">
+			<div className="row page-title-spacing">
+				<div className="col-12">
+					<h2 className="text-center slight-shadow">Contact Page Title</h2>
+				</div>
+			</div>
+
+			<div className="row">
+				<div className="col-12">
+					<p className="text-center section-bottom-spacing">If you have any
+						questions, comments or concerns, feel free to use the form below to
+						contact me!</p>
+				</div>
+			</div>
+
+			<ContactForm />
 		</div>
 	);
 }
