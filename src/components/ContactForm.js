a7173eac9c4ecdac9b2ae58763bd3ed6fe32fb6e
Eric Korber <erickorber1994@gmail.com>
2018-06-02 12:18:38 -0400

    Add HTML, CSS to contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,9 +2,31 @@ import React from 'react';
 
 const ContactForm = () => {
 	return (
-		<div>
-		[ Contact Form Component ]
-		</div>
+		<form id="contact-form" action="sendMessage" modelAttribute="email">
+			<div className="form-row">
+				<div className="col-12 col-md-6">
+					<input type="text" className="form-control mb-3" placeholder="First Name"/>
+				</div>
+				<div className="col-12 col-md-6">
+					<input type="email" className="form-control mb-3" placeholder="Email"/>
+				</div>
+			</div>
+			<div className="form-row">
+				<div className="col-12">
+					<input type="text" className="form-control mb-3" placeholder="Subject"/>
+				</div>
+			</div>
+			<div className="form-row">
+				<div className="col-12">
+					<textarea className="form-control mb-3" placeholder="Message" rows="5"></textarea>
+				</div>
+			</div>
+			<div className="form-row">
+				<div className="col-12">
+					<button type="submit" class="btn btn-lg btn-primary btn-block mb-2">Send Message</button>
+				</div>
+			</div>
+		</form>
 	);
 }
 
