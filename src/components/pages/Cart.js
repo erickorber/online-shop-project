4fb1252714ffcb18b48d9b85311aa1511ffa8dea
Eric Korber <erickorber1994@gmail.com>
2018-06-30 17:28:34 -0400

    Create cart page

---
+++ src/components/pages/Cart.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import { products } from '../../products.js';
+
+const Cart = ({productIdArray}) => {
+
+	const cartProducts = [];
+
+	for (let i = 0; i < productIdArray.length; i++) {
+
+		const checkId = (jsonProduct) => {
+			return jsonProduct.id === productIdArray[i][0];
+		}
+
+		cartProducts.push([products.find(checkId), productIdArray[i][1]]);
+	}
+
+	const displayItemsInCart = (list) => {
+		
+		const tableRows = [];
+		
+		for (let i = 0; i < list.length; i++) {
+			tableRows.push(
+				<tr key={i}>
+					<th scope="row">{list[i][0].name}</th>
+					<td className="text-center">{list[i][1]}</td>
+					<td className="text-center">${list[i][0].price.toFixed(2)}</td>
+				</tr>
+			);
+		}
+
+		return tableRows;
+	}
+
+	const displayTotalPrice = (list) => {
+
+		let totalPrice = 0;
+
+		for (let i = 0; i < list.length; i++) {
+			totalPrice += (list[i][0].price * list[i][1]);
+		}
+
+		return totalPrice.toFixed(2);
+	}
+
+	return (
+		<div className="container">
+			<div className="row page-title-spacing">
+				<div className="col-12">
+					<h2 className="text-center slight-shadow">Your Cart</h2>
+				</div>
+			</div>
+
+			<div className="row">
+				<div className="col-12">
+					<table className="table table-bordered table-striped">
+					  	<thead>
+					    	<tr>
+							    <th scope="col">Product</th>
+							    <th scope="col" className="text-center">Quantity</th>
+							    <th scope="col" className="text-center">Price</th>
+					    	</tr>
+					  	</thead>
+					  	<tbody>
+					  		{displayItemsInCart(cartProducts)}
+					  		<tr>
+					  			<th scope="row" colSpan="2">Total</th>
+					  			<td className="text-center">${displayTotalPrice(cartProducts)}</td>
+					  		</tr>
+					  	</tbody>
+					</table>	
+				</div>
+			</div>
+			<div className="row my-2">
+				<div className="col-8 offset-2">
+					<button className="btn btn-primary btn-block" type="button" value="add cart">PayPal Placeholder</button>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Cart;
