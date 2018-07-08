01b82e40988f6b1846c750a32f94ead93460dc10
Eric Korber <erickorber1994@gmail.com>
2018-07-08 10:33:04 -0400

    Setup routing in index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import { logger } from 'redux-logger';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import { rootReducer } from './reducers.js';
+import { Route, Switch } from 'react-router';
+import { createBrowserHistory } from 'history';
+import { ConnectedRouter, connectRouter, routerMiddleware } from 'connected-react-router';
 
-const store = createStore(rootReducer, applyMiddleware(logger));
+const history = createBrowserHistory();
+
+const store = createStore(
+	connectRouter(history)(rootReducer),
+	compose(applyMiddleware(routerMiddleware(history), logger))
+);
 
 ReactDOM.render(<Provider store = {store}>
-				  <App />
+					<ConnectedRouter history={history}>
+				  		<div> { /* your usual react-router v4 routing */ }
+					        <Switch>
+					        	<Route exact path="/" component={App} />
+					        </Switch>
+					    </div>
+				  	</ConnectedRouter>
 				</Provider>,
 				document.getElementById('root'));
 registerServiceWorker();
