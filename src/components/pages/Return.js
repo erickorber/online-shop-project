39f77b8c231268448a2d554ad4bfce5b52006fbd
Eric Korber <erickorber1994@gmail.com>
2018-09-19 23:09:49 -0400

    Clear cart on successful transaction

--- src/components/pages/Return.js
+++ src/components/pages/Return.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { updateCart } from '../../actions.js';
 
-const Return = (props) => {
-	return (
-		<div className="container">
-			<div className="row page-title-spacing">
-				<div className="col-12">
-					<h2 className="text-center slight-shadow">Transaction successful! Thank you for your purchase!</h2>
-				</div>
-			</div>
-		</div>
-	);
+//This is what the state currently is
+const mapStateToProps = (state) => {
+	return {
+   		cartReduxState: state.user.cartItems
+	}
 }
 
-export default Return;
+//This is for when you'd like to update the state
+const mapDispatchToProps = (dispatch) => {
+  	return {
+    	dispatchUpdatedCart: (newCart) => dispatch(updateCart(newCart))
+  	}
+}
+
+class Return extends Component {
+	
+	componentDidMount() {
+		this.props.dispatchUpdatedCart([]);
+	}
+
+	render() {
+		return (
+			<div className="container">
+				<div className="row page-title-spacing">
+					<div className="col-12">
+						<h2 className="text-center slight-shadow">Transaction successful! Thank you for your purchase!</h2>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Return);
