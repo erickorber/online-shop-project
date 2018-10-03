43ace326cadcac3cc75821e9b2cb52fa5fde6e91
Eric Korber <erickorber1994@gmail.com>
2018-10-03 14:50:50 -0400

    Undo last push

--- src/ContactForm/data.js
+++ src/ContactForm/data.js
@@ -2,7 +2,7 @@ import { SERVER_ADDRESS } from '../constants.js';
 
 const data = {
 	"serverAddress" : SERVER_ADDRESS + "/contact-form-submit",
-	"recaptchaSiteKey" : process.env.RECAPTCHA_SITE_KEY
+	"recaptchaSiteKey" : "6LctMm4UAAAAAEVrlrUy2HLjPIJEWugx7TzF7jef"
 }
 
 export default data;
