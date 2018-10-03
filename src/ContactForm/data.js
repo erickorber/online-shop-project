aec9ca30cb5e093ada12422ad4f4bc7ce72e39af
Eric Korber <erickorber1994@gmail.com>
2018-10-03 14:46:09 -0400

    Change recaptcha site key to Heroku config var

--- src/ContactForm/data.js
+++ src/ContactForm/data.js
@@ -2,7 +2,7 @@ import { SERVER_ADDRESS } from '../constants.js';
 
 const data = {
 	"serverAddress" : SERVER_ADDRESS + "/contact-form-submit",
-	"recaptchaSiteKey" : "6LctMm4UAAAAAEVrlrUy2HLjPIJEWugx7TzF7jef"
+	"recaptchaSiteKey" : process.env.RECAPTCHA_SITE_KEY
 }
 
 export default data;
