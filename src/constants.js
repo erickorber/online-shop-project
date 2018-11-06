3c24e0fb49995af8a6c8a3a13f430408dd4f264b
Eric Korber <erickorber1994@gmail.com>
2018-11-06 17:02:53 -0500

    Update server address

--- src/constants.js
+++ src/constants.js
@@ -8,5 +8,5 @@ export const REQUEST_PRODUCT_FAILED = 'REQUEST_PRODUCT_FAILED';
 export const REQUEST_CART_LIST_PENDING = 'REQUEST_CART_LIST_PENDING';
 export const REQUEST_CART_LIST_SUCCESS = 'REQUEST_CART_LIST_SUCCESS';
 export const REQUEST_CART_LIST_FAILED = 'REQUEST_CART_LIST_FAILED';
-export const SERVER_ADDRESS = "https://xephyrium-shop-server.herokuapp.com";
+export const SERVER_ADDRESS = "https://xephyriumshop.com/api";
 export const DEFAULT_CURRENCY = "USD";
