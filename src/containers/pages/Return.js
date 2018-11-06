ec13b29629f09e782238ecb2048fc2dbe297d7a5
Eric Korber <erickorber1994@gmail.com>
2018-11-06 09:15:26 -0500

    Update successful transaction text

--- src/containers/pages/Return.js
+++ src/containers/pages/Return.js
@@ -27,7 +27,8 @@ class Return extends Component {
 			<div className="container">
 				<div className="row page-title-spacing">
 					<div className="col-12">
-						<h2 className="text-center slight-shadow">Transaction successful! Thank you for your purchase!</h2>
+						<h2 className="text-center slight-shadow">Transaction successful! 
+						Your receipt has been emailed to you. Thank you for your purchase!</h2>
 					</div>
 				</div>
 			</div>
